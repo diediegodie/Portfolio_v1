0af85c1b6e543c1e2db0e8486e7ceccbd32c61d7
Diego Rodriguez <diegotattoosp@gmail.com>
2025-12-23 16:15:36 -0300

    homepage minimalist approach

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,7 +1,1 @@
-// Load HTMX
-const script = document.createElement('script');
-script.src = 'https://unpkg.com/htmx.org@1.9.10';
-script.defer = true;
-document.head.appendChild(script);
-
 // Custom JS logic here
